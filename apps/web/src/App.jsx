4f512b3e8691975c75ea5c7b9d9835891bbc82e1
Daniips <d.pombero@gmail.com>
2025-10-19 12:37:18 +0200

    Landing page

--- apps/web/src/App.jsx
+++ apps/web/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { SessionProvider } from "./context/SessionContext";
+import Landing from "./pages/Landing";
 import Lobby from "./pages/Lobby";
 import Vote from "./pages/Vote";
 import JoinSession from "./pages/JoinSession";
@@ -10,7 +11,8 @@ export default function App() {
     return (
         <SessionProvider>
             <Routes>
-                <Route path="/" element={<Lobby />} />
+                <Route path="/" element={<Landing />} />
+                <Route path="/create" element={<Lobby />} />
                 <Route path="/vote" element={<Vote />} />
                 <Route path="/s/:id" element={<JoinSession />} />
             </Routes>
